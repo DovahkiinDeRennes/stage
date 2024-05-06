9b91903e3bb032438f96b21feba2cf5b81ea2f61
Your Name <you@example.com>
2024-05-06 11:14:52 +0200

    produit + cateegorie dans la fonctionnalité

--- src/pages/admin/categories/categorieWithServiceProduit.php
+++ src/pages/admin/categories/categorieWithServiceProduit.php
@@ -3,6 +3,8 @@ include(__DIR__ . '/../../core/connection.php');
 
 require_once __DIR__ . '/../../../classes/service.php';
 
+require_once __DIR__ . '/../../../classes/produit.php';
+
 $service = new Service($db);
 
 // Vérifie si une catégorie a été sélectionnée
@@ -35,5 +37,36 @@ if (isset($_GET['categorie_id'])) {
         echo "<tr><td><a href='/info.php?id=" . $row['id'] . "&amp;titre=" . htmlspecialchars($row['titre']) . "'>" . htmlspecialchars($row['titre']) . "</a></td></tr>";
     }
 }
+$produit = new produit($db);
 
+// Vérifie si une catégorie a été sélectionnée
+if (isset($_GET['categorie_id'])) {
+    $categorie_id = htmlspecialchars($_GET['categorie_id']);
+    echo "Catégorie sélectionnée : " . $categorie_id;
+    // ...
+
+
+    // Récupère les services pour la catégorie spécifiée
+    $query = "SELECT * FROM produits WHERE categories = :categorie_id";
+    $stmt = $db->prepare($query);
+    $stmt->bindParam(':categorie_id', $categorie_id, PDO::PARAM_INT);
+    $stmt->execute();
+    $produit = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    // Affiche les services correspondant à la catégorie sélectionnée
+    foreach ($produit as $produit => $row) {
+        echo "<br><tr><td><a href='/info.php?id=" . $row['id'] . "&amp;titre=" . htmlspecialchars($row['titre']) . "'>" . htmlspecialchars($row['titre']) . "</a></td></tr><br>";
+    }
+} else {
+    // Si aucun paramètre 'categorie_id' n'est défini dans l'URL, affiche tous les services
+    $query = "SELECT * FROM produits";
+    $stmt = $db->prepare($query);
+    $stmt->execute();
+    $services = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    // Affiche tous les services
+    foreach ($produit as $produit => $row) {
+        echo "<tr><td><a href='/info.php?id=" . $row['id'] . "&amp;titre=" . htmlspecialchars($row['titre']) . "'>" . htmlspecialchars($row['titre']) . "</a></td></tr>";
+    }
+}
 ?>
