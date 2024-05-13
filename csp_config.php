c5415b4f063d962e4e062da6d28f0d1b488fddd7
Your Name <you@example.com>
2024-05-13 15:31:14 +0200

    Commit CSP

--- csp_config.php
+++ csp_config.php
@@ -17,7 +17,7 @@ $nonce7 = bin2hex(random_bytes(16));
 
 // Définir la directive CSP avec les nonces
 $csp_directive = "default-src 'self' $decrypted_url1 ; ";
-$csp_directive .= "script-src 'self'  $decrypted_url1 $decrypted_url5 $decrypted_url8 'nonce-" . $nonce1 . "' 'nonce-" . $nonce2 . "' 'nonce-" . $nonce3 . "' 'nonce-" . $nonce4 . "' 'nonce-" . $nonce5 . "' 'nonce-" . $nonce6 . "' 'nonce-" . $nonce7 . "' ; ";
+$csp_directive .= "script-src 'self' $decrypted_url1 $decrypted_url5 $decrypted_url8 'nonce-" . $nonce1 . "' 'nonce-" . $nonce2 . "' 'nonce-" . $nonce3 . "' 'nonce-" . $nonce4 . "' 'nonce-" . $nonce5 . "' 'nonce-" . $nonce6 . "' 'nonce-" . $nonce7 . "' ; ";
 $csp_directive .= "style-src 'self' 'unsafe-inline' $decrypted_url1 $decrypted_url2 $decrypted_url3; ";
 $csp_directive .= "connect-src 'self' $decrypted_url4 $decrypted_url5; ";
 $csp_directive .= "font-src 'self' $decrypted_url6 $decrypted_url3 $decrypted_url4 $decrypted_url5; ";
